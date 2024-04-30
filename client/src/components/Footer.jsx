e80761ffd0abc3f3edcebdb7868d0dd8f94d8737
PRAKRITI KUMAR <166092037+Prakxsg@users.noreply.github.com>
2024-05-01 02:18:41 +0530

    Create Footer.jsx

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo from "../img/logo.jpg"
+
+const Footer = () => {
+  return (
+    <footer>
+      <img src={Logo} alt=""/>
+      <span>Bloggers have the write idea, they just can't seem to find the right pun!</span>
+    </footer>
+  )
+}
+export default Footer
